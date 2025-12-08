a8285df6f303cf577e03908fcd4f079e58041cc7
Abdalrhman Mohamed <abdalrhnanmohamed1912005@gmail.com>
2025-12-08 15:23:35 +0200

    setup database connection

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+// IMPORTS
 const expreass = require("express");
 const dotenv = require("dotenv");
+const { connectDB } = require("./config/dbConfig");
+// GLOBAL CONFIG
 dotenv.config();
 
 const app = expreass();
-
+// GLOBAL PORT
 const PORT = process.env.PORT || 3000
-
+// ROUTES
 app.get("/", (req, res) => {
     res.send("Welcome to the server API");
 });
 
+connectDB();
+
+
+// LISTENER 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
