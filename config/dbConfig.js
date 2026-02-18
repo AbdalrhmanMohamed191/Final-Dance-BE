e940dae1443d3b183eef019822f65eb6b3255a86
AbdAlrhman Mohamed <abdalrhnanmohamed1912005@gmail.com>
2026-02-18 16:30:31 +0200

    Final Project

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,16 +1,36 @@
-// IMPORTS
+// // IMPORTS
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+// // CONFIG
+// dotenv.config();
+// // CONNECTION TO MONGODB
+// async function connectDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URI);
+//         console.log("Connected to MongoDB");
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//     }
+// }
+
+// module.exports = {connectDB};
+
+
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-// CONFIG
-dotenv.config();
-// CONNECTION TO MONGODB
+
+let isConnected = false;
+
 async function connectDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to MongoDB");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-    }
+  if (isConnected) return;
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState;
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
 }
 
-module.exports = {connectDB};
+module.exports = { connectDB };
